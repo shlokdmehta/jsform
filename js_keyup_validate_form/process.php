7da0b9c486efaeb529666316aca1cd5bb683c717
Shlok Mehta <shlokdmehta@gmail.com>
2018-03-02 15:09:58 -0500

    V2.1
    Error solved due to the string reference and naming conventions.

--- js_keyup_validate_form/process.php
+++ js_keyup_validate_form/process.php
@@ -5,27 +5,25 @@
 		}else if(strpbrk($_REQUEST['sname'], '123456789') == TRUE){
 			echo "Student name can not contain digits";
 		} else if($_REQUEST['sname'] == ""){
-			echo "Empty";
+			echo "Empty-sname";
 		}
 	}
-
 	if(isset($_REQUEST['sid'])){
 		if($_REQUEST['sid'] == "000000000" || $_REQUEST['sid'] == "111111111" || $_REQUEST['sid'] == "222222222" || $_REQUEST['sid'] == "333333333" || $_REQUEST['sid'] == "444444444" || $_REQUEST['sid'] == "555555555" || $_REQUEST['sid'] == "666666666" || $_REQUEST['sid'] == "777777777" || $_REQUEST['sid'] == "888888888" || $_REQUEST['sid'] == "999999999"){
 			echo "Student ID is suspicious";
 		} else if(strpbrk($_REQUEST['sid'], 'abcdefghijklmnopqrstuvwxyz') == TRUE){
 			echo "Student ID must contain only digits";
 		} else if($_REQUEST['sid'] == ""){
-			echo "Empty2";
+			echo "Empty-sid";
 		}
 	}
-
 	if(isset($_REQUEST['program'])){
 		if($_REQUEST['program'] == "Basket Weaving" || $_REQUEST['program'] == "Beanstalk Climbing"){
 			echo "Program may be fictious";
 		} else if ($_REQUEST['program'] == "Geology" || $_REQUEST['program'] == "Microbiology") {
 			echo "Program does not exist";
 		} else if ($_REQUEST['program'] == "") {
-			echo "Empty3";
+			echo "Empty-program";
 		}
 	} 
 ?>
