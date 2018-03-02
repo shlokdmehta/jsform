29de864ab3cb14cd7b46dc55a2d2dfa0556c8a1a
Shlok Mehta <shlokdmehta@gmail.com>
2018-03-02 15:11:31 -0500

    V2.1
    Error due to the naming conventions and string reference solved

--- js_keyup_validate_form/keyupval.js
+++ js_keyup_validate_form/keyupval.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 					$("#label-name-msg").html("Student name my be fictious");
 					$("#std-name").removeClass().addClass('form-group has-warning has-feedback');
 					$("#glyphicon-name").removeClass().addClass('glyphicon glyphicon-warning-sign form-control-feedback');
-				}else if(data == "Student name cannot contain digits"){
+				}else if(data == "Student name can not contain digits"){
 					$("#label-name-msg").html("Student name cannot contain digits");
 					$("#std-name").removeClass().addClass('form-group has-error has-feedback');
 					$("#glyphicon-name").removeClass().addClass('glyphicon glyphicon-remove form-control-feedback');
-				}else if(data == "Empty"){
+				}else if(data == "Empty-sname"){
 					$("#label-name-msg").html("").removeClass();
 					$("#std-name").removeClass();
 					$("#glyphicon-name").removeClass();
@@ -25,7 +25,7 @@ $(document).ready(function(){
 	$("#sid").keyup(function(){
 		$.post("process.php",{"sid": $("#sid").val()},
 			function(data2){
-				if (data2 == "Student ID must contain only dogits"){
+				if (data2 == "Student ID must contain only digits"){
 					$("#label-num-msg").html("Student ID must only contain digits");
 					$("#std-num").removeClass().addClass('form-group has-error has-feedback');
 					$("#glyphicon-num").removeClass().addClass('glyphicon glyphicon-remove-sign form-control-feedback');
@@ -33,7 +33,7 @@ $(document).ready(function(){
 					$("#label-num-msg").html("Student ID is suspicious");
 					$("#std-num").removeClass().addClass('form-group has-warning has-feedback');
 					$("#glyphicon-num").removeClass().addClass('glyphicon glyphicon-warning-sign form-control-feedback');
-				}else if(data2 == "Empty2"){
+				}else if(data2 == "Empty-sid"){
 					$("#label-num-msg").html("").removeClass();
 					$("#std-num").removeClass();
 					$("#glyphicon-num").removeClass();
@@ -48,15 +48,15 @@ $(document).ready(function(){
 	$("#pro").keyup(function(){
 		$.post("process.php",{"program": $("#program").val()},
 			function (data3) {
-				if(data3 == "Program may be fictious."){
+				if(data3 == "Program may be fictious"){
 					$("label-program").html("Program may be ficitious.");
 					$("#std-program").addClass('form-group has-warning has-feedback');
 					$("#glyphicon-program").addClass('glyphicon glyphicon-warning-sign form-control-feedback');
-				}else if(data3 == "program does not exist"){
+				}else if(data3 == "Program does not exist"){
 					$("label-program").html("Program does not exist.");
 					$("#std-program").addClass('form-group has-error has-feedback');
 					$("#glyphicon-program").addClass('glyphicon glyphicon-remove form-control-feedback');
-				}else if(data3 == "Empty3"){
+				}else if(data3 == "Empty-program"){
 					$("label-program").html("").removeClass();
 					$("#std-program").removeClass().addClass('form-group has-success has-feedback');
 					$("#glyphicon-program").removeClass().addClass('glyphicon glyphicon-ok form-control-feedback');
@@ -86,7 +86,7 @@ $(document).ready(function(){
 			$("#std-program").removeClass();
 			$("#glyphicon-program").removeClass();
 			
-			
+		
 
 	}else{
 		var tabel = "<tr>";
